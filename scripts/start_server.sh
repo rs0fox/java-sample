c59f7ad8c6853984e7e72cf4f21ce6c09a0364df
TestUser <testuser@example.com>
2024-07-04 12:42:35 +0530

    test files added

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # Example: Start Java application
-
-APP_HOME=/opt/myapp  # Replace with your application's home directory
-JAR_FILE=/path/to/your/application.jar  # Replace with the actual path to your application JAR file
-MAIN_CLASS=com.example.Main  # Replace with your main class
-
-# Change directory to the application's home directory
-cd $APP_HOME || exit
-
-# Start the Java application
-java -cp $JAR_FILE $MAIN_CLASS > /var/log/myapp.log 2>&1 &
+java -classpath /tmp/my-java-app/src com.example.Main
